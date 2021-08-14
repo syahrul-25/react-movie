03f115a381723fcc1da9d1062a40e5491059ad7a
syahrul-25 <pas25kata@gmail.com>
2021-08-15 02:31:20 +0900

    crate global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { API_KEY } from "./config";
+
+// styles
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
-  return <div className="App">{API_KEY}</div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      Start Here
+    </div>
+  );
 }
 
 export default App;
