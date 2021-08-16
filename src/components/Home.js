785b389229501d9f945c1987f01436b1756befee
syahrul-25 <pas25kata@gmail.com>
2021-08-16 22:04:28 +0900

    Create Hero Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 // Config
-import { POSTER_SIZE, BACKDROP, IMAGE_BASE_URL } from "../config";
+import {
+  POSTER_SIZE,
+  BACKDROP,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+} from "../config";
 // components
+import HeroImage from "./HeroImages";
 
 // Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 // Image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state?.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
