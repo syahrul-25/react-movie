fbb9781ab5f5a37a3a34072278ec1c7d82cd1b63
syahrul-25 <pas25kata@gmail.com>
2021-08-17 02:14:03 +0900

    create component breadcump and movie info

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,8 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 // components
 import Grid from "./Grid";
 import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
@@ -15,10 +17,12 @@ const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
     </>
   );
 };
