3eae7192696389d19ff3c046a1eead2f651ff1f3
syahrul-25 <pas25kata@gmail.com>
2021-08-17 01:50:33 +0900

    create useState Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,26 @@
 import React from "react";
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+// components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+// Hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
 
-const Movie = () => <div>Movie</div>;
+// Image
+import NoImage from "../images/no_image.jpg";
+import { useParams } from "react-router";
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  console.log(movie);
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect, useCallback } from "react";
+
+import API from "../API";
+
+export const useMovieFetch = (movieId) => {
+  const [state, setState] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(false);
+
+      const movie = await API.fetchMovie(movieId);
+      const credits = await API.fetchCredits(movieId);
+      // get directors only
+      const directors = credits.crew.filter(
+        (member) => member.job === "Director"
+      );
+
+      setState({
+        ...movie,
+        actors: credits.cast,
+        directors,
+      });
+
+      setLoading(false);
+    } catch (e) {
+      setError(true);
+    }
+  }, [movieId]);
+
+  useEffect(() => {
+    fetchData();
+  }, [movieId, fetchData]);
+
+  return { state, loading, error };
+};
