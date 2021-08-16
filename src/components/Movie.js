d8415f3495ebcc60ffa10cdecb6da5dfcba48955
syahrul-25 <pas25kata@gmail.com>
2021-08-17 03:01:20 +0900

    create and style component actors

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,7 @@ import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "../Actor";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
@@ -29,6 +30,21 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header="Actors">
+        {console.log(movie)}
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
